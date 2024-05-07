9bf607750adaa832a1405a63ae4d8d14f7a0afbe
Haris Sabanovic <hasa2303@student.miun.se>
2024-05-07 15:24:00 +0200

    form för att konvertera meter till feet

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.component.html',
   styleUrl: './convert.component.scss'
 })
