943f18077ae09198cdf1ae60c5b4061ad1397490
Haris Sabanovic <hasa2303@student.miun.se>
2024-05-07 14:47:57 +0200

    klassen hanterar konvertering av värden mellan olika enheter

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -9,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class ConvertComponent {
 
+  meterValue: number = 0;
+  feetValue: number = 0;
+  celsiusValue: number = 0;
+  fahrenheitValue: number = 0;
+
+  convertMetertoFeet() {
+    this.feetValue = this.meterValue * 3.28084;
+  }
+
+  convertFeettoMeter() {
+    this.meterValue = this.feetValue / 3.28084;
+  }
+
+  convertCelsiustoFahrenheit(){
+    this.fahrenheitValue = (this.celsiusValue * 9/5) + 32;
+  }
+
+  convertFahrenheitToCelsius() {
+    this.celsiusValue = (this.fahrenheitValue - 32) * 5/9;
+  }
+
 }
